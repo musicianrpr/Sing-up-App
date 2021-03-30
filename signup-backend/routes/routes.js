ac9881627b75721b702dba6bf7c8a67710e198e4
Lucas Gabriel de Melo Rodrigues <lucasgabrielmelorodrigues@gmail.com>
2021-03-30 15:43:56 -0300

    added hash and salt password feature

--- signup-backend/routes/routes.js
+++ signup-backend/routes/routes.js
@@ -2,13 +2,18 @@ const { response } = require('express')
 const express = require('express')
 const router = express.Router()
 const signUpTemplateCopy = require('../models/signup-models')
+const bcrypt = require('bcrypt')
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+
+  const saltPassword = await bcrypt.genSalt(10)
+  const securePassword = await bcrypt.hash(req.body.password, saltPassword)
+
   const signedUpUser = new signUpTemplateCopy({
     fullName: req.body.fullName,
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: securePassword,
   })
   signedUpUser.save()
   .then(data => {
